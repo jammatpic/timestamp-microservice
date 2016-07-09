b7b1ec458efd2b2e3c0916ed15afcee66f76a525
jammatpic <jammatpic@gmail.com>
2016-07-10 00:27:27 +1000

    Added main functionality where dates are translated

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require("express");
+var moment = require("moment");
 var app = express();
 
+
 app.get('/:dateTime', function (req, res) {
-    res.end(req.params.dateTime);
+    var theDate = moment(req.params.dateTime, ["X", "MMMM DD, YYYY"]);
+    var dateJSON = {"unix": theDate.unix(), "natural": theDate.format("MMMM DD, YYYY")};
+    res.end(JSON.stringify(dateJSON));
 });
 
 app.get('/', function (req, res) {
